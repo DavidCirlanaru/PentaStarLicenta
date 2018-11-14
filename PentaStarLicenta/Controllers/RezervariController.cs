37639d1992e334ac4a864b1a32d6f25babe000ac
David Cirlanaru <cirlanaru.david@gmail.com>
2018-11-14 12:01:52 +0200

    Incercat sa implementez formular cu post in baza de date. Fail big time.

--- PentaStarLicenta/Controllers/RezervariController.cs
+++ PentaStarLicenta/Controllers/RezervariController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PentaStarLicenta.DAL.Models;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace PentaStarLicenta.Controllers
 {
@@ -12,6 +17,37 @@ namespace PentaStarLicenta.Controllers
         public ActionResult Index()
         {
             return View();
+
         }
+
+        //private SqlConnection con; 
+
+        //private void connection()
+        //{
+        //    string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+        //    con = new SqlConnection(constr);
+        //}
+
+        //[HttpPost]
+        //public ActionResult AddReservationRequest (Client clients)
+        //{
+        //    try
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //             = new Client();
+                   
+
+                   
+        //        }
+
+        //        return View();
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
     }
 }
