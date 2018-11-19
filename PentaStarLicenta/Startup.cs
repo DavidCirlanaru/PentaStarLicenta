60d2ac12bfe004811f8ee17ce42c6bec2da918e4
David Cirlanaru <cirlanaru.david@gmail.com>
2018-11-19 16:48:06 +0200

    Creating a default admin and user roles

--- PentaStarLicenta/Startup.cs
+++ PentaStarLicenta/Startup.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using PentaStarLicenta.DAL.Context;
+using PentaStarLicenta.Models;
 
-[assembly: OwinStartupAttribute(typeof(PentaStarLicenta.Startup))]
+
 namespace PentaStarLicenta
 {
     public partial class Startup
@@ -9,6 +13,56 @@ namespace PentaStarLicenta
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // Creating first Admin Role and creating a default Admin User
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // Create Admin role
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Create a Admin super user who will maintain the website                        
+                var user = new ApplicationUser();
+                user.UserName = "Admin";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "Admin123#";
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            // Creating Manager role
+            if (!roleManager.RoleExists("Receptionist"))
+            {
+
+                var role = new IdentityRole();
+                role.Name = "Receptionist";
+                roleManager.Create(role);
+            }
+
+            // creating Employee role
+            //if (!roleManager.RoleExists("Employee"))
+            //{
+            //    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            //    role.Name = "Employee";
+            //    roleManager.Create(role);
+            //}
         }
     }
 }
