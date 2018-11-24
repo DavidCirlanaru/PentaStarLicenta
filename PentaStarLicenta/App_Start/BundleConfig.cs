2ad9d22570cea8df05c4adefa7df7f9d7f169bee
David Cirlanaru <cirlanaru.david@gmail.com>
2018-11-24 13:06:34 +0200

    Update to Bootsrap 4

--- PentaStarLicenta/App_Start/BundleConfig.cs
+++ PentaStarLicenta/App_Start/BundleConfig.cs
@@ -35,7 +35,8 @@ namespace PentaStarLicenta
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                "~/Scripts/umd/popper.js",
+                "~/Scripts/bootstrap.js"));
 
         }
 
